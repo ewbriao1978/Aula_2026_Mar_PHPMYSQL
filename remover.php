f53a7eae48aa46cd305b1b354bb01aa201456dab
ewbriao1978 <eduardo.briao@gmail.com>
2026-03-13 09:47:23 -0300

    wee

---
+++ remover.php
@@ -0,0 +1,24 @@
+<?php
+
+    $id = $_POST["id_para_ser_removido"];
+
+    $conexao = mysqli_connect("localhost", "root", "", "aulaprog");
+
+    if (!$conexao) {
+        die("Falha na conexão: " . mysqli_connect_error());
+    }
+
+    $query = "DELETE FROM my_table WHERE id = $id";
+    //echo "Query SQL: " . $query . "<br>";
+
+    if (mysqli_query($conexao, $query)) {
+        echo "Produto removido com sucesso!";
+    } else {
+        echo "Erro ao remover produto: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    header('Location: home.php')
+
+?>
